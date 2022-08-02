b64cfba00e5bf3f398e74116359bc97545d53288
YJCCreateAHistory <cnjc_19991222@163.com>
2022-08-02 17:27:17 +0800

    [feat]社交信息数据请求

--- src/components/socials/api.ts
+++ src/components/socials/api.ts
@@ -3,12 +3,15 @@ import {PARAMS} from "./index"
 export const getSocialsList = ():Promise<PARAMS>=>{
     return GetRequest("/socials/info")
 }
+
 export const updateSocialsList = (params:PARAMS):Promise<PARAMS>=>{
     return PostRequest("/socials/update", params) 
 }
+
 export const createSocialsList = (params:PARAMS):Promise<PARAMS>=>{
     return PostRequest("/socials/update", params) 
 }
+
 export const deleteSocialsList = (params:PARAMS):Promise<PARAMS>=>{
     return  PostRequest("/socials/update", params) 
 }
