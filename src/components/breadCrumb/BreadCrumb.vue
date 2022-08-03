9911ee9c4371cc7892d56af1d086bc0468ce6dd4
YJCCreateAHistory <cnjc_19991222@163.com>
2022-08-03 22:51:08 +0800

    [feat]更新了博客面包屑导航与菜单联动

---
+++ src/components/breadCrumb/BreadCrumb.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <a-breadcrumb>
+      <a-breadcrumb-item v-for="(item, index) in list" :key="item.path"
+        ><router-link :to="item.path"
+          >{{ item.meta.title }}
+        </router-link>
+        </a-breadcrumb-item>
+    </a-breadcrumb>
+
+</template>
+
+<script setup lang="ts">
+import { computed } from "@vue/reactivity";
+import { useRoute } from "vue-router";
+const router = useRoute();
+const list = computed(() => {
+  return router.matched;
+});
+</script>
+
+<style scoped lang="less"></style>
